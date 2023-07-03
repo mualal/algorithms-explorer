76d178d96d26f834951c5afd7d4f59593eaf2a35
mualal <almuravcev@yandex.ru>
2023-07-03 21:19:58 +0300

    c:practice: add burger fervor with memoization and dynamic programming

--- c/practice/07_burger_fervor/burger_fervor_memoization.c
+++ c/practice/07_burger_fervor/burger_fervor_memoization.c
@@ -10,42 +10,59 @@ int max(int v1, int v2) {
     }
 }
 
-int solve_t(int m, int n, int t) {
+unsigned long long total_calls;
+
+int solve_t(int m, int n, int t, int memo[]) {
     int first, second;
+    total_calls++;
+    if (memo[t] != -2) {
+        return memo[t];
+    }
     if (t == 0) {
-        return 0;
+        memo[t] = 0;
+        return memo[t];
     }
     if (t >= m) {
-        first = solve_t(m, n, t - m);
+        first = solve_t(m, n, t - m, memo);
     } else {
         first = -1;
     }
     if (t >= n) {
-        second = solve_t(m, n, t - n);
+        second = solve_t(m, n, t - n, memo);
     } else {
         second = -1;
     }
     if (first == -1 && second == -1) {
-        return -1;
+        memo[t] = -1;
+        return memo[t];
     } else {
-        return max(first, second) + 1;
+        memo[t] = max(first, second) + 1;
+        return memo[t];
     }
 }
 
+#define SIZE 10000
+
 void solve(int m, int n, int t) {
     int result, i;
-    result = solve_t(m, n, t);
+    total_calls = 0;
+    int memo[SIZE];
+    for (i = 0; i <= t; i++) {
+        memo[i] = -2;
+    }
+    result = solve_t(m, n, t, memo);
     if (result >= 0) {
         printf("%d\n", result);
     } else {
         i = t - 1;
-        result = solve_t(m, n, i);
+        result = solve_t(m, n, i, memo);
         while (result == -1) {
             i--;
-            result = solve_t(m, n, i);
+            result = solve_t(m, n, i, memo);
         }
         printf("%d %d\n", result, t - i);
     }
+    printf("Total calls to solve_t: %llu\n", total_calls);
 }
 
 int main(void) {
