0e37dad01a30cbe0d2dbf40d6a86bc87ae8d38fa
mualal <almuravcev@yandex.ru>
2023-07-06 11:03:48 +0300

    c:practice: add hockey rivalry with memoization and dynamic programming

--- c/practice/09_hockey_rivalry/hockey_rivalry_memoization.c
+++ c/practice/09_hockey_rivalry/hockey_rivalry_memoization.c
@@ -10,32 +10,37 @@ int max(int v1, int v2) {
     }
 }
 
+#define SIZE 1000
 unsigned long long total_calls = 0;
 
-int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int i, int j) {
+int solve(char outcome1[], char outcome2[], int goals1[], int goals2[], int i, int j, int memo[SIZE + 1][SIZE + 1]) {
     int first, second, third, fourth;
     total_calls++;
+    if (memo[i][j] != -1) {
+        return memo[i][j];
+    }
     if (i == 0 || j == 0) {
-        return 0;
+        memo[i][j] = 0;
+        return memo[i][j];
     }
     if ((outcome1[i] == 'W' && outcome2[j] == 'L' && goals1[i] > goals2[j]) || 
         (outcome1[i] == 'L' && outcome2[j] == 'W' && goals1[i] < goals2[j])) {
-        first = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1) + goals1[i] + goals2[j];
+        first = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo) + goals1[i] + goals2[j];
     } else {
         first = 0;
     }
-    second = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1);
-    third = solve(outcome1, outcome2, goals1, goals2, i - 1, j);
-    fourth = solve(outcome1, outcome2, goals1, goals2, i, j - 1);
-    return max(first, max(second, max(third, fourth)));
+    second = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo);
+    third = solve(outcome1, outcome2, goals1, goals2, i - 1, j, memo);
+    fourth = solve(outcome1, outcome2, goals1, goals2, i, j - 1, memo);
+    memo[i][j] = max(first, max(second, max(third, fourth)));
+    return memo[i][j];
 }
 
-#define SIZE 1000
-
 int main(void) {
-    int i, n, result;
+    int i, j, n, result;
     char outcome1[SIZE + 1], outcome2[SIZE + 1];
     int goals1[SIZE + 1], goals2[SIZE + 1];
+    static int memo[SIZE + 1][SIZE + 1];
     scanf("%d ", &n);
     for (i = 1; i <= n; i++) {
         scanf("%c", &outcome1[i]);
@@ -49,7 +54,12 @@ int main(void) {
     for (i = 1; i <= n; i++) {
         scanf("%d", &goals2[i]);
     }
-    result = solve(outcome1, outcome2, goals1, goals2, n, n);
+    for (i = 0; i <= SIZE; i++) {
+        for (j = 0; j <= SIZE; j++) {
+            memo[i][j] = -1;
+        }
+    }
+    result = solve(outcome1, outcome2, goals1, goals2, n, n, memo);
     printf("%d\n", result);
     printf("Total calls to solve_t: %llu\n", total_calls);
     return 0;
